76a75e7e62d8e4490c3a8fd6d41c55de67ab156d
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-06-21 14:37:48 +0530

    Home page added

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,7 +1,21 @@
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
-export default HomePage
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/form");
+  };
+
+  return (
+    <div className="flex flex-col gap-10 items-center justify-center w-full max-w-3xl p-4 sm:p-6 md:p-8 lg:p-10 mx-auto h-screen">
+      <h2 className="font-mono text-2xl font-semibold">Job Application Form</h2>
+      <Button onClick={handleClick} variant="default">
+        Click here to Register
+      </Button>
+    </div>
+  );
+};
+
+export default HomePage;
